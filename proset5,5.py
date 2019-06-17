72e3e1ff7d12fd882e84e2ab0d20b2c864979424
KIRTHIKABABU <46591020+KIRTHIKABABU@users.noreply.github.com>
2019-06-17 22:11:04 +0530

    Rename proset5,4.py to proset5,5.py

---
+++ proset5,5.py
@@ -0,0 +1,14 @@
+#kd2805
+t=input()
+if t==t[::-1]:
+    print("yes")
+else:
+    value=t.strip("0")
+    if value==value[::-1]:
+        print("yes")
+    else:
+        value=t.lstrip("0")
+        if value==value[::-1]:
+            print("yes")
+        else:
+            print("no")
